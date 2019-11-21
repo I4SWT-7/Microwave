0f4a97f792e5aff371d387adb93348cfb4c40ed9
Mathias <mathiasholsko@hotmail.com>
2019-11-21 12:47:15 +0100

    Adding integration test project

---
+++ Microwave.test.integration/IT1_CookController_Display.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microwave.test.integration
+{
+    public class IT1_Cookcontroller_display
+    {
+    }
+}
