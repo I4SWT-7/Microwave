e05ed1121f99ab910b35aefc1df2fe9c2d7b380d
Andreas Ventzel <au280318@uni.au.dk>
2019-11-21 15:55:50 +0100

    IT1 tests

--- Microwave.test.integration/IT1_CookController_Display.cs
+++ Microwave.test.integration/IT1_CookController_Display.cs
@@ -3,11 +3,53 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Timers;
+using Castle.Core.Smtp;
+using Microsoft.Win32;
+using MicrowaveOvenClasses;
+using MicrowaveOvenClasses.Boundary;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
+using NUnit.Framework;
+using NSubstitute;
 
 namespace Microwave.test.integration
 {
+    [TestFixture]
     public class IT1_Cookcontroller_display
     {
+        private IDisplay _display;
+        private ITimer _timer;
+        private IPowerTube _powertube;
+        private IOutput _output;
+        private CookController _uut;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _display = new Display(_output);
+            _timer = Substitute.For<ITimer>();
+            _powertube = Substitute.For<IPowerTube>();
+            _output = Substitute.For<IOutput>();
+            _uut = new CookController(_timer,_display,_powertube);
+
+        }
+
+        [Test]
+        public void StartCooking_ShowTime()
+        {
+            _uut.StartCooking(50,30);
+            _output.Received(1).OutputLine($"Display shows: 50 W");
+        }
+
+
+        //[Test]
+        //public void StartCooking_ShowPower()
+        //{
+
+        //}
+
 
     }
+    
 }
