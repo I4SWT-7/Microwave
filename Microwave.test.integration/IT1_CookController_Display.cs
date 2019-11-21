3b676a5c146e651da6d35fe43ebf570f5bc03bf6
Andreas Ventzel <au280318@uni.au.dk>
2019-11-21 18:17:40 +0100

    IT1 done

--- Microwave.test.integration/IT1_CookController_Display.cs
+++ Microwave.test.integration/IT1_CookController_Display.cs
@@ -23,33 +23,33 @@ namespace Microwave.test.integration
         private IPowerTube _powertube;
         private IOutput _output;
         private CookController _uut;
+        private TestTimer _testTimer;
 
         [SetUp]
         public void SetUp()
         {
+            _output = Substitute.For<IOutput>();
+            _testTimer = new TestTimer();
             _display = new Display(_output);
             _timer = Substitute.For<ITimer>();
             _powertube = Substitute.For<IPowerTube>();
-            _output = Substitute.For<IOutput>();
-            _uut = new CookController(_timer,_display,_powertube);
+            
+            _uut = new CookController(_testTimer,_display,_powertube);
+
 
         }
 
         [Test]
         public void StartCooking_ShowTime()
         {
-            _uut.StartCooking(50,60);
-            _timer.TimeRemaining.Returns(30);
-            _timer.TimerTick += Raise.EventWith(this, EventArgs.Empty);
+            
+            _uut.StartCooking(50,30);
+            //_timer.TimeRemaining.Returns(30);
+            
+            _testTimer.RaiseEvent();
             _output.Received().OutputLine(Arg.Is<string>(str => str.Contains("00:30")));
         }
 
-
-        //[Test]
-        //public void StartCooking_ShowPower()
-        //{
-
-        //}
 
 
     }
---
+++ MicrowaveOvenClasses/Boundary/TestTimer.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MicrowaveOvenClasses.Interfaces;
+
+namespace MicrowaveOvenClasses.Boundary
+{
+    public class TestTimer : ITimer
+    {
+        public int TimeRemaining { get; private set; }
+        public event EventHandler Expired;
+        private System.Timers.Timer timer;
+        public event EventHandler TimerTick;
+
+        public TestTimer()
+        {
+            timer = new System.Timers.Timer();
+        }
+        public void RaiseEvent()
+        {
+            TimerTick?.Invoke(
+                this,
+                EventArgs.Empty);
+        }
+
+        public void Start(int time)
+        {
+            TimeRemaining = time;
+            timer.Enabled = true;
+
+        }
+
+        public void Stop()
+        {
+
+        }
+
+        public void Expire()
+        {
+
+        }
+    }
+}
