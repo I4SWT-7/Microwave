3d659ba1c797cc645e26b20b3bc571a8689f31e1
MikkelMahler <mmmikkel@hotmail.com>
2019-11-25 15:51:28 +0100

    Started IT6

--- Microwave.test.integration/IT6_Cookcontroller_Userinterface.cs
+++ Microwave.test.integration/IT6_Cookcontroller_Userinterface.cs
@@ -3,12 +3,33 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Controllers;
+using MicrowaveOvenClasses.Interfaces;
 using NSubstitute;
 using NUnit;
+using NUnit.Framework;
+using NUnit.Framework.Internal;
 
 namespace Microwave.test.integration
 {
+    [TestFixture]
     class IT6_Cookcontroller_Userinterface
     {
+        private ICookController _uut;
+        private IUserInterface _userInterface;
+        private ILight _light;
+        private IDisplay _display;
+        private ITimer _timer;
+        private IPowerTube _powerTube;
+        private IOutput _output;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _output = Substitute.For<IOutput>();
+
+            _userInterface = new UserInterface(_output);
+        }
+
     }
 }
