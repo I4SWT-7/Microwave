2e2f799f5013240d4e0f4fdbe3c574b6a6798543
MikkelMahler <mmmikkel@hotmail.com>
2019-11-26 12:23:48 +0100

    Finished IT6

--- Microwave.test.integration/IT6_Cookcontroller_Userinterface.cs
+++ Microwave.test.integration/IT6_Cookcontroller_Userinterface.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MicrowaveOvenClasses.Boundary;
 using MicrowaveOvenClasses.Controllers;
 using MicrowaveOvenClasses.Interfaces;
 using NSubstitute;
@@ -22,13 +23,44 @@ namespace Microwave.test.integration
         private ITimer _timer;
         private IPowerTube _powerTube;
         private IOutput _output;
+        private IButton _powerButton;
+        private IButton _timeButton;
+        private IButton _start_cancel_button;
+        private IDoor _door;
 
         [SetUp]
         public void SetUp()
         {
+            //Substitutes
             _output = Substitute.For<IOutput>();
+            _door = Substitute.For<IDoor>();
+            _powerButton = Substitute.For<IButton>();
+            _timeButton = Substitute.For<IButton>();
+            _start_cancel_button = Substitute.For<IButton>();
 
-            _userInterface = new UserInterface(_output);
+            //Real
+            _light = new Light(_output);
+            _display = new Display(_output);
+            _timer = new Timer();
+            _powerTube = new PowerTube(_output);
+            _userInterface = new UserInterface(_powerButton, _timeButton, _start_cancel_button, _door, _display,
+                _light, _uut);
+            _uut = new CookController(_timer, _display, _powerTube);
+        }
+
+
+        [Test]
+        public void CookingIsDone_Received_inUserInterface()
+        {
+            _powerButton.Pressed += Raise.Event();
+            _timeButton.Pressed += Raise.Event();
+            _start_cancel_button.Pressed += Raise.Event();
+
+            _timer.Expired += Raise.Event();
+
+            _display.Received().Clear();
+            _light.Received().TurnOff();
+
         }
 
     }
